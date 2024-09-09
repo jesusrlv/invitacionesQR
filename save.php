461636406e25cf96f10023e86272bd166a308552
jesusrlv <jesusrlvrojo@gmail.com>
2024-09-09 16:31:31 -0600

    acomodos previo 4

--- save.php
+++ save.php
@@ -1,27 +1,36 @@
 <?php
     include('conn.php');
 
+    $tipoInvitacion = $_POST['tipoInvitacion'];
     $nombre = $_POST['nombre'];
-    $curp = $_POST['curp'];
+    $municipios = $_POST['municipios'];
+    $edad = $_POST['edad'];
+    $fecha = $_POST['fecha'];
     $email = $_POST['email'];
     $telefono = $_POST['telefono'];
     $evento = 1;
     $checkin = 0;
 
     $sql = "INSERT INTO invitacion (
-        nombre,
-        curp,
-        telefono,
-        email,
-        evento,
-        checkin) 
+            tipoInvitacion,
+            nombre,
+            municipio,
+            edad,
+            fechaNacimiento,
+            telefono,
+            email,
+            evento,
+            checkin
         VALUES (
-            '$nombre',
-            '$curp',
-            '$telefono',
-            '$email',
-            '$evento',
-            '$checkin'
+        '$tipoInvitacion',
+        '$nombre',
+        '$municipios',
+        '$edad',
+        '$fecha',
+        '$email',
+        '$telefono',
+        '$evento',
+        '$checkin'
         )";
     $resultado = $conn->query($sql);
     
