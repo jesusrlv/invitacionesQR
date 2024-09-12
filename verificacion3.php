2b9bcf3a62e783fc228f34de3e9d688c014bf2d8
jesusrlv <jesusrlvrojo@gmail.com>
2024-09-12 09:40:14 -0600

    cambios 2

--- verificacion3.php
+++ verificacion3.php
@@ -1,6 +1,6 @@
 <?php 
 require('conn.php');
-sleep(1);
+// sleep(1);
 if (isset($_POST)) {
     $username = (string)$_POST['username'];
  
@@ -13,14 +13,28 @@ if (isset($_POST)) {
         <span class="badge text-bg-danger"><small><i class="bi bi-x-circle-fill"></i> Usuario registrado anteriormente </small><span>
         <script>
             document.getElementById("registroBtn").disabled=true;
+           Swal.fire({
+                icon: \'error\',
+                title: \'Error\',
+                html: \'Usuario registrado anteriormente\',
+                showConfirmButton: false,
+                timer: 2000
+            });
         </script>
         ';
 
     } else {
         echo '
-        <span class="badge text-bg-primary"><small><i class="bi bi-check-circle-fill"></i> Usuario no registrado anteriormente</small><span> 
+        <span class="badge text-bg-primary"><small><i class="bi bi-check-circle-fill"></i> Usuario disponible</small><span> 
         <script>
             document.getElementById("registroBtn").disabled=false;
+            Swal.fire({
+                icon: \'success\',
+                title: \'Disponible\',
+                html: \'Usuario disponible\',
+                showConfirmButton: false,
+                timer: 2000
+            });
         </script>
         ';
     }
